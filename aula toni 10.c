49fc762bc35b0b39f108c2dc26a6f387107aff13
Yumi <148996314+KarenYumi@users.noreply.github.com>
2024-03-27 18:54:33 -0300

    Add files via upload
    Com alterações mais corretas, porém repete os números do vetor inteiro

--- aula toni 10.c
+++ aula toni 10.c
@@ -5,34 +5,30 @@
 int main(){
     setlocale(LC_ALL, "Portuguese");
 
-    int num[10], i, x,n=0, contador=0, newVetor[10];
+    int num[10], i, x, contador=0, newVetor[10];
 
     printf("Digite 10 números:\n");
     for(i=1; i<=10; i++){
         scanf("%d", &num[i]);
     }
 
-    for(i=1; i<=10;i++){
-        for(x=i+1;x<=10;x++){
+    for(i=1; i<=10; i++){
+        contador = 0;
+        for(x=1; x<=10; x++){
             if(num[i] == num[x]){
-                newVetor[n++] = num[i];
                 contador++;
+                newVetor[i] = contador;
             }
-
         }
     }
-    for(i=1; i<=10;i++){
+    for(i=1; i<=10; i++){
         printf("%d ", num[i]);
 
     }
     printf("\nValores repetidos: \n");
-    for(i = 1; i<n; i++){
-      printf("%d repetiu %d vezes\n", newVetor[i], contador);
+    for(i = 1; i<=10 ; i++){
+      printf("%d repetiu %d vezes\n", num[i], newVetor[i]);
    }
-
-
-
-
 
 
 return 0;
